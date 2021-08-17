5eec7976c020eaa20949cfbcfdbb574d98162dad
treyer <nemereno_by@mail.ru>
2021-08-17 23:55:26 +0300

    feat: decode function implemented

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let wardsArr = expr.split('**********');
+    let res = '';
+    for (let i = 0; i < wardsArr.length; i++){
+        if (res.length > 0) res += ' ';
+
+        if (wardsArr[i].length % 10 !== 0){
+            wardsArr[i] = wardsArr[i].substring(0, -10) + "0" + wardsArr[i].substring(-9);
+        }
+
+        let symbolsArray = [];
+        for (let j = 0; j < wardsArr[i].length / 10; j++){
+            symbolsArray.push(wardsArr[i].substring(j * 10, j * 10 + 10));
+        }
+
+        for (let m = 0; m < symbolsArray.length; m++) {
+            res = res + symbolTranslateFromDigits(symbolsArray[m]);
+        }
+    }
+    return res;
+}
+
+let symbolTranslateFromDigits = (symbolInDigits) => {
+    let symbolMorse = '';
+    for (let j = 0; j < symbolInDigits.length; j += 2){
+        if (symbolInDigits.substring(j, j + 2) === "10"){
+            symbolMorse += '.';
+        } else if (symbolInDigits.substring(j, j + 2) === "11"){
+            symbolMorse += '-';
+        }
+    }
+    return MORSE_TABLE[symbolMorse];            
 }
 
 module.exports = {
